f2abd7e04840da97247151377c42e7167bda7c87
ThanhHai <thanhhaitran1203@gmail.com>
2024-03-04 20:42:25 +0700

    CRUD JPA

---
+++ src/main/java/com/customermanagementjpa/repository/ICustomerRepository.java
@@ -0,0 +1,6 @@
+package com.customermanagementjpa.repository;
+
+import com.customermanagementjpa.model.Customer;
+
+public interface ICustomerRepository extends IGenerateRepository<Customer>{
+}
---
+++ src/main/java/com/customermanagementjpa/service/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.customermanagementjpa.service;
+
+import com.customermanagementjpa.model.Customer;
+
+public interface ICustomerService extends IGenerateService<Customer>{
+
+}
